74db8a370e3430fa1454f366e5d265f213b77053
CAMILA PADILHA <camilapadilha1997@gmail.com>
2019-12-31 18:27:19 -0300

    Refactoring 2: Rename

--- src/aulapraticarefactoring/Customer.java
+++ src/aulapraticarefactoring/Customer.java
@@ -29,23 +29,23 @@ class Customer {
         return _name;
     }
 
-    public double amout(Rental each) {
+    public double amout(Rental aRental) {
         double thisAmount = 0;
         //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
+        switch (aRental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2) {
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (aRental.getDaysRented() > 2) {
+                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
                 }
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += aRental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3) {
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (aRental.getDaysRented() > 3) {
+                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
                 }
                 break;
         }
