5d17b716aa318906b46f5a158b6c09aafa5ee3ef
CAMILA PADILHA <camilapadilha1997@gmail.com>
2019-12-31 19:09:44 -0300

    Refactoring 4: Replace Temp with Query

--- src/aulapraticarefactoring/Customer.java
+++ src/aulapraticarefactoring/Customer.java
@@ -39,11 +39,7 @@ class Customer {
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
-            thisAmount = each.getCharge();
-
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -54,8 +50,8 @@ class Customer {
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t"
-                    + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
 
         }
         //add footer lines
